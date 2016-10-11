e45b2b557efc98f3bd4b6b149fc48a0b06727ad3
kkw <kkw@gimgeon-uui-MacBook-Air.local>
2016-10-11 09:03:55 +0900

    init

---
+++ httpd.c
@@ -0,0 +1,200 @@
+/* J. David's webserver */
+/* This is a simple webserver.
+ * Created November 1999 by J. David Blackstone.
+ * CSE 4344 (Network concepts), Prof. Zeigler
+ * University of Texas at Arlington
+ */
+/* This program compiles for Sparc Solaris 2.6.
+ * To compile for Linux:
+ *  1) Comment out the #include <pthread.h> line.
+ *  2) Comment out the line that defines the variable newthread.
+ *  3) Comment out the two lines that run pthread_create().
+ *  4) Uncomment the line that runs accept_request().
+ *  5) Remove -lsocket from the Makefile.
+ */
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <strings.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+#define ISspace(x) isspace((int)(x))
+
+#define SERVER_STRING "Server: jdbhttpd/0.1.0\r\n"
+
+void accept_request(int);
+int get_line(int, char *, int);
+void headers(int);
+int startup(u_short *);
+void error_die(const char*);
+
+void error_die(const char *sc)
+{
+	 perror(sc);
+	  exit(1);
+}
+
+/**********************************************************************/
+/* A request has caused a call to accept() on the server port to
+ * return.  Process the request appropriately.
+ * Parameters: the socket connected to the client */
+/**********************************************************************/
+void accept_request(int client)
+{
+ char buf[1024];
+ int numchars;
+
+ numchars = get_line(client, buf, sizeof(buf));
+ while ((numchars > 0) && strcmp("\n", buf))
+	 numchars = get_line(client, buf, sizeof(buf));
+ headers(client);
+ close(client);
+}
+
+int get_line(int sock, char *buf, int size)
+{
+ int i = 0;
+ char c = '\0';
+ int n;
+
+ while ((i < size - 1) && (c != '\n'))
+ {
+  n = recv(sock, &c, 1, 0);
+  /* DEBUG printf("%02X\n", c); */
+  if (n > 0)
+  {
+   if (c == '\r')
+   {
+    n = recv(sock, &c, 1, MSG_PEEK);
+    /* DEBUG printf("%02X\n", c); */
+    if ((n > 0) && (c == '\n'))
+     recv(sock, &c, 1, 0);
+    else
+     c = '\n';
+   }
+   buf[i] = c;
+   i++;
+   printf("%c",c);
+  }
+  else
+   c = '\n';
+ }
+ buf[i] = '\0';
+ 
+ return(i);
+}
+
+/**********************************************************************/
+/* Return the informational HTTP headers about a file. */
+/* Parameters: the socket to print the headers on
+ *             the name of the file */
+/**********************************************************************/
+void headers(int client)
+{
+ char buf[1024];
+
+ strcpy(buf, "HTTP/1.0 200 OK\r\n");
+ send(client, buf, strlen(buf), 0);
+ printf("%s",buf);
+ strcpy(buf, SERVER_STRING);
+ send(client, buf, strlen(buf), 0);
+ printf("%s",buf);
+ sprintf(buf, "Content-Type: text/html\r\n");
+ send(client, buf, strlen(buf), 0);
+ printf("%s",buf);
+ strcpy(buf, "\r\n");
+ send(client, buf, strlen(buf), 0);
+ printf("%s",buf);
+}
+
+int startup(u_short *port)
+{
+ int httpd = 0;
+ struct sockaddr_in name;
+
+ httpd = socket(PF_INET, SOCK_STREAM, 0);
+ if (httpd == -1)
+  error_die("socket");
+ memset(&name, 0, sizeof(name));
+ name.sin_family = AF_INET;
+ name.sin_port = htons(*port);
+ name.sin_addr.s_addr = htonl(INADDR_ANY);
+ if (bind(httpd, (struct sockaddr *)&name, sizeof(name)) < 0)
+  error_die("bind");
+ if (*port == 0)  /* if dynamically allocating a port */
+ {
+  int namelen = sizeof(name);
+  if (getsockname(httpd, (struct sockaddr *)&name, &namelen) == -1)
+   error_die("getsockname");
+  *port = ntohs(name.sin_port);
+ }
+ if (listen(httpd, 5) < 0)
+  error_die("listen");
+ return(httpd);
+}
+
+/**********************************************************************/
+/* Inform the client that the requested web method has not been
+ * implemented.
+ * Parameter: the client socket */
+/**********************************************************************/
+void unimplemented(int client)
+{
+ char buf[1024];
+
+ sprintf(buf, "HTTP/1.0 501 Method Not Implemented\r\n");
+ send(client, buf, strlen(buf), 0);
+ sprintf(buf, SERVER_STRING);
+ send(client, buf, strlen(buf), 0);
+ sprintf(buf, "Content-Type: text/html\r\n");
+ send(client, buf, strlen(buf), 0);
+ sprintf(buf, "\r\n");
+ send(client, buf, strlen(buf), 0);
+ sprintf(buf, "<HTML><HEAD><TITLE>Method Not Implemented\r\n");
+ send(client, buf, strlen(buf), 0);
+ sprintf(buf, "</TITLE></HEAD>\r\n");
+ send(client, buf, strlen(buf), 0);
+ sprintf(buf, "<BODY><P>HTTP request method not supported.\r\n");
+ send(client, buf, strlen(buf), 0);
+ sprintf(buf, "</BODY></HTML>\r\n");
+ send(client, buf, strlen(buf), 0);
+}
+
+/**********************************************************************/
+
+int main(void)
+{
+ int server_sock = -1;
+ u_short port = 80;
+ int client_sock = -1;
+ struct sockaddr_in client_name;
+ int client_name_len = sizeof(client_name);
+ pthread_t newthread;
+
+ server_sock = startup(&port);
+ printf("httpd running on port %d\n", port);
+
+ while (1)
+ {
+  client_sock = accept(server_sock,
+                       (struct sockaddr *)&client_name,
+                       &client_name_len);
+  if (client_sock == -1)
+   error_die("accept");
+ /* accept_request(client_sock); */
+ if (pthread_create(&newthread , NULL, accept_request, client_sock) != 0)
+   perror("pthread_create");
+ }
+
+ close(server_sock);
+
+ return(0);
+}
